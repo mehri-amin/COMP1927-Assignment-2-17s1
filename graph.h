3d945d2054f850c3009c59ab1e2bc22e370b3c5b
Simon Shields <simon@lineageos.org>
2017-05-16 20:33:48 +1000

    split graph.c and graph.h

---
+++ graph.h
@@ -0,0 +1,21 @@
+// graph.h ... Interface to Graph of strings
+// Written by John Shepherd, September 2015
+// Taken from lab 8: https://cgi.cse.unsw.edu.au/~cs1927/17s1/labs/week08/
+
+#ifndef GRAPH_H
+#define GRAPH_H
+
+typedef struct GraphRep *Graph;
+
+// Function signatures
+
+Graph newGraph(int);
+void  disposeGraph(Graph);
+int   addEdge(Graph,char *,char *);
+int   nVertices(Graph);
+int   isConnected(Graph, char *, char *);
+void  showGraph(Graph,int);
+int outDegree(Graph, char *);
+
+#endif
+
