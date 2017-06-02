6d7def0791e25423fc22fe48a5368b8ac7b59684
mehri-amin <mehriamin@Mehris-MacBook-Pro.local>
2017-06-02 13:52:28 +1000

    Added comments

--- pagerank.c
+++ pagerank.c
@@ -1,3 +1,15 @@
+/* 
+   COMP1927 Assignment 2
+   Simple Search Engines
+
+   Group: Mehri Amin, Simon Shields, Stanley Seng Chi
+   
+   
+   Part-1 (A) Calculate PageRanks
+   (8 Marks)
+
+*/   
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -7,21 +19,27 @@
 #include "llist.h"
 #include "graph.h"
 
-#define EPSILON 1E-6
+#define EPSILON 1E-6 // To compare float numbers
 
+// Struct for each url
 typedef struct URL{
-	double pagerank;
-	int nOutgoing;
-	char *name;
+	double pagerank; // page rank value 
+	int nOutgoing; // number of outgoing links
+	char *name; // name of url
 } *URL;
 
+// Function to calculate page rank of each url
 void calculatePageRank(Graph,List,double, double, int);
+// Function to sort page ranks then put results in to file "pagerankList.txt"
 void OutputToFile(Graph,List, double *);
 
+
+// Compares page rank values for ordering
 static int ComparePageRank(const void *a,const void *b){
 	return (((URL)b)->pagerank - ((URL)a)->pagerank > EPSILON) ? 0 : 1;
 }
 
+// Compares nOutgoing of urls for further ordering
 static int CompareOutgoing(const void *a,const void *b){
 
 	return ((((URL)b)->nOutgoing - ((URL)a)->nOutgoing) > 0) ? 0 : 1;
@@ -39,7 +57,7 @@ int main(int argc, char *argv[]){
 	int maxIterations;
 	d = atof(argv[1]);
 	diffPR = atof(argv[2]);
-	maxIterations = atoi(argv[3]);
+	maxIterations = atoi(argv[3]); 
 
 	// GetCollection & GetGraph
 	List l = GetCollection();
@@ -47,19 +65,24 @@ int main(int argc, char *argv[]){
 	Graph g = GetGraph();
 	showGraph(g,0);
 
+	// Calculates and Orders page ranks
+	// then outputs to "pagerankList.txt"
 	calculatePageRank(g,l,d,diffPR,maxIterations);
 
+	//Free memory 
 	disposeGraph(g);
 	destroyList(l);
 
 	return EXIT_SUCCESS;
 }
 
+// Simplified PageRank Algorithm
 void calculatePageRank(Graph g,List l, double d, double diffPR, int maxIterations){
-	int N = nVertices(g);
+	int N = nVertices(g); // N = number of urls in the collection
 	double sum, diff, PR[N], oldPR[N];
 
-	int i=0,j=0;
+	int i,j; // represents urls and urls in file
+
 	// for each url PR[i] in the collection
 	for(i=0;i<N;i++)
 		PR[i] = oldPR[i] = 1.0f/(double) N;
@@ -72,7 +95,7 @@ void calculatePageRank(Graph g,List l, double d, double diffPR, int maxIteration
 		memcpy(oldPR, PR, sizeof(double) * N);
 		diff = 0;
 		for (i=0;i<N;i++){
-			sum = 0; // initialize sum
+			sum = 0; 
 			for(j=0;j<N;j++){
 				if (i == j) continue;
 				if (isConnected(g,g->vertex[j],g->vertex[i])) { // check if there is a connection between page i and j
@@ -83,9 +106,11 @@ void calculatePageRank(Graph g,List l, double d, double diffPR, int maxIteration
 			diff += fabs(PR[i] - oldPR[i]); // convergence is assumed
 		}
 	}
+	// Order and sned output to file
 	OutputToFile(g,l, PR);
 }
 
+// Order and send ordered output to "pagerankList.txt"
 void OutputToFile(Graph g, List l, double *PR){
 	int i=0;
 	int N=nVertices(g);
